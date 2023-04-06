5400837785132a6b650b200460676e49f889b2ab
Rivo <yosef.nicklaus@findours.com>
2023-04-07 04:31:41 +0700

    d

--- index.js
+++ index.js
@@ -43,49 +43,40 @@ async function bot(prx) {
 
 
     async function run(bw) {
+        const browser = bw
         const page = await bw.newPage(); // create a new page
         await page.setDefaultNavigationTimeout(0);// r
         page.waitForNavigation({waitUntil: 'networkidle2', timeout: 0}).then()
         await page.goto('https://www.pinksale.finance/launchpad/0x22F69A8960b823147bCD055E2E2598Aa0BC82F1C?chain=BSC', {timeout: 0}); // navigate to the website
-        if (bw && page) {
-            setInterval(async () => {
 
-                const pgs = await bw.pages();
-                for (let i = 1; i < pgs.length; i++) {
-                    if (i >= 2) {
-                        await pgs[i].close().then(() => {
-                            console.log("CLOSED TAB")
-                        });
+
+        await page.evaluate(() => {
+            setTimeout(() => {
+                setInterval(() => {
+                    for (let i = 0; i < 10; i++) {
+                        document.querySelectorAll(".media-content a")[i].dispatchEvent(new MouseEvent('click', {ctrlKey: true}))
                     }
+                    document.querySelectorAll(".ant-typography.has-text-primary")[0].click()
+                    document.querySelectorAll(".ant-typography.has-text-primary")[1].click()
+                    document.querySelectorAll(".ant-typography.has-text-primary")[2].click()
+                    console.log(`--------- RUNNING >>>> [ ${pr} ] <<<< RUNNING ---------`)
+
+                }, 15000)
+
+            }, 500)
+        });
+
+        setInterval(async () => {
+            const pages = await browser.pages();
+            for (let i = 1; i < pages.length; i++) {
+                if (i >= 2) {
+                    await pages[i].close();
+                    console.log(`--------- TAB CLOSED >>>> [ ${pr} ] <<<< TAB CLOSED ---------`)
+
                 }
-            }, 20000);
-            await page.evaluate(() => {
-                let num = 1;
-                setTimeout(() => {
-                    running()
+            }
+        }, 5000);
 
-                    function running() {
-                        console.log("------------------ RUNNING ----------------")
-
-                        for (let i = 0; i < 10; i++) {
-                            document.querySelectorAll(".media-content a")[i].dispatchEvent(new MouseEvent('click', {ctrlKey: true}))
-
-                        }
-                        document.querySelectorAll(".ant-typography.has-text-primary")[0].click()
-                        document.querySelectorAll(".ant-typography.has-text-primary")[1].click()
-                        document.querySelectorAll(".ant-typography.has-text-primary")[2].click()
-                    }
-
-                    setInterval(() => {
-                        running()
-                    }, 70000)
-                }, 500)
-
-
-            });
-        } else {
-            console.log("CLOSED")
-        }
     }
 }
 
