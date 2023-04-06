85927b379329c9df479dba528c9aef1f7a0fedc3
Rivo <yosef.nicklaus@findours.com>
2023-04-07 04:45:42 +0700

    as

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 const puppeteer = require("puppeteer");
 const input = require("input");
 const proxyList = [
-  "89.179.244.233:20000",
-  "185.143.42.54:8080",
-  "5.78.82.94:8080",
-  "114.231.46.188:8089",
+  undefined,
+  "5.78.70.253:8080",
+  "66.94.108.138:3128",
+  undefined,
 ];
 
 function bot({ pr }) {
   (async () => {
-    const browser = await puppeteer.launch({
-      headless: false,
+    const dataPr = {
       args: [
         `--proxy-server=http://${pr}`,
         "--no-sandbox",
         "--ignore-certificate-errors",
         "--ignore-certificate-errors-spki-list",
       ],
-    }); // launch the browse
+    };
+    const dataSetting = pr
+      ? {
+          headless: false,
+          ...dataPr,
+        }
+      : {
+          headless: false,
+        };
+    const browser = await puppeteer.launch(dataSetting); // launch the browse
     const page = await browser.newPage(); // create a new page
     await page.setDefaultNavigationTimeout(0); // r
     page.waitForNavigation({ waitUntil: "networkidle0" }).then();
     await page.goto(
-      "https://www.pinksale.finance/launchpad/0xD9f3C050465f118E0BE854697008D71a412b3e1d?chain=BSC",
+      "https://www.pinksale.finance/launchpad/0x22F69A8960b823147bCD055E2E2598Aa0BC82F1C?chain=BSC",
       { timeout: 260000 }
     ); // navigate to the website
     // await page.goto("https://rivopelu.my.id/", { timeout: 0 }); // navigate to the website
-
-    await page.evaluate((e) => {
-      alert("RUNNING");
+    setInterval(() => {
+      browser.close();
+    }, 60000);
+    await page.evaluate(() => {
       setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          document.querySelectorAll(".media-content a")[i].click();
+        }
         document
           .querySelectorAll(".ant-typography.has-text-primary")[0]
           .click();
         document
           .querySelectorAll(".ant-typography.has-text-primary")[1]
           .click();
-        for (let i = 0; i < 10; i++) {
-          document.querySelectorAll(".media-content a")[i].click();
-        }
+        document
+          .querySelectorAll(".ant-typography.has-text-primary")[2]
+          .click();
       }, 1000);
     });
 
@@ -71,7 +83,7 @@ function myLoop(linkAmount) {
     } else {
       i = 0;
     }
-  }, 20000);
+  }, 30000);
 }
 
 function main() {
